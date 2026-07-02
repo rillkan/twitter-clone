14ccdbba574c71edb7013f20eedd239fc92a9cdd
Reagan Kan <mjkan96@gmail.com>
2026-07-02 11:34:33 +0800

    add firebase config

--- src/components/NewPostModel.jsx
+++ src/components/NewPostModel.jsx
@@ -1,37 +1,27 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { jwtDecode } from "jwt-decode";
+import {AuthContext} from './AuthProvider'
 
 export default function NewPostModal({ show, handleClose }) {
   const [postContent, setPostContent] = useState("");
+  const [file, setFile ] = useState(null);
+  const {currentUser, savePost} = useContext(AuthContext);
+  const userId = currentUser?.uid
 
   const handleSave = () => {
-    //Get stored JWT Token
-    const token = localStorage.getItem("authToken");
-
-    //Decode the token to fetch user id
-    const decode = jwtDecode(token);
-    const userId = decode.id; // May change depending on how the server encode the token
-
-    //Prepare data to be sent
-    const data = {
-      title: "Post Title",  //Add functionality to set this properly
-      content: postContent,
-      user_id: userId,
-    };
-
-    //Make your API call here
-    axios
-      .post("http://localhost:3000/posts", data)
-      .then((response) => {
-        console.log("Success:", response.data);
-        handleClose();
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
+    if(userId){
+      savePost(userId, postContent, file);
+      handleClose();
+      setPostContent('')
+      setFile(null)
+    }
   };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  }
 
   return (
     <>
@@ -44,8 +34,11 @@ export default function NewPostModal({ show, handleClose }) {
                 placeholder="What is happening?!"
                 as="textarea"
                 rows={3}
+                value={postContent}
                 onChange={(e) => setPostContent(e.target.value)}
               />
+              <br/>
+              <Form.Control type="file" onChange={handleFileChange}/>
             </Form.Group>
           </Form>
         </Modal.Body>
--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,18 +1,37 @@
-import { useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
+import { AuthContext } from "./AuthProvider";
+import UpdatePostModal from "./UpdatePostModal"
 
-export default function ProfilePostCard({content,postId}) {
-  const [likes, setLikes] = useState(0)
+export default function ProfilePostCard({ post}) {
+  const { content, id: postId, likes: postLikes = [], imageUrl } = post;
+  const { currentUser, likePost, removeLikeFromPost, deletePost } = useContext(AuthContext);
+  const userId = currentUser?.uid;
+  const [likes,setLikes] = useState(postLikes || [])
+  const [showUpdateModal, setShowUpdateModal] = useState(false)
+
+  const isLiked = likes.includes(userId);
+
   const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
 
-  useEffect(()=>{
-    fetch(
-      `http://localhost:3000/likes/post/${postId}`
-    )
-    .then((response)=> response.json())
-    .then((data)=>setLikes(data.length))
-    .catch((error)=>console.error("Error", error))
-  }, [postId])
+  const handleShowUpdateModal = () => setShowUpdateModal(true);
+  const handleCloseUpdateModal = () => setShowUpdateModal(false);
+
+  const handleLike = () => {
+    if (!userId) return;
+    if (isLiked) {
+      setLikes(likes.filter((id)=> id !== userId))
+      removeLikeFromPost(userId, postId);
+    } else {
+      setLikes([...likes, userId])
+      likePost(userId, postId);
+    }
+  };
+
+  const handleDelete = ()=>{
+    if(!userId) return;
+    deletePost(userId,postId);
+  }
 
   return (
     <Row
@@ -30,6 +49,7 @@ export default function ProfilePostCard({content,postId}) {
         <strong>Haris</strong>
         <span> @haris.samingan · Apr 16</span>
         <p>{content}</p>
+        {imageUrl && <Image src={imageUrl} style={{width:150}}/>}
         <div className="d-flex justify-content-between">
           <Button variant="light">
             <i className="bi bi-chat"></i>
@@ -37,18 +57,32 @@ export default function ProfilePostCard({content,postId}) {
           <Button variant="light">
             <i className="bi bi-repeat"></i>
           </Button>
-          <Button variant="light">
-            <i className="bi bi-heart">{likes}</i>
+          <Button variant="light" onClick={handleLike}>
+            {isLiked ? (
+              <i className="bi bi-heart-fill text-danger"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
+            {likes.length}
           </Button>
           <Button variant="light">
             <i className="bi bi-graph-up"></i>
           </Button>
-          <Button variant="light">
-            <i className="bi bi-upload"></i>
+          <Button variant="light" onClick={handleShowUpdateModal}>
+            <i className="bi bi-pencil-square"></i>
+          </Button>
+          <Button variant="light" onClick={handleDelete}>
+            <i className="bi bi-trash"></i>
           </Button>
         </div>
+
+        <UpdatePostModal
+          show={showUpdateModal}
+          handleClose={handleCloseUpdateModal}
+          postId={postId}
+          originalPostContent={content}
+        />
       </Col>
     </Row>
-  )
+  );
 }
-
